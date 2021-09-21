79d2f684f7733cbc0064da6df3e698a043a956ea
Sumitbashetwar <84890708+Sumitbashetwar@users.noreply.github.com>
2021-09-21 12:48:39 +0530

    Assignment 1 Submitted

---
+++ Assignment_1/ASSIGNMENT-1_PRGRAM.py
@@ -0,0 +1,5 @@
+principle = int(input("Enter the value of PRINCIPLE:-\t"))
+rate = int(input("Enter the value of RATE:-\t"))
+time = int(input("Enter the value of time:-\t"))
+S_I = (principle * rate * time)/100
+print("The Simple Interest is :- %.2f"%float(S_I))
